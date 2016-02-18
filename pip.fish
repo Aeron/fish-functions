658ac3c5e1cb7441a4c9411456623a555df36740
Eugene “Aeron” Glybin <aeron@aeron.cc>
2016-02-18 18:13:55 +0300

    Pip wrapper added.

---
+++ pip.fish
@@ -0,0 +1,18 @@
+function pip -d 'Handy wrapper for OS X pip'
+	set -l pip_version (command pip --version)
+	set -l cmd
+
+	switch $pip_version 
+		case '* /Library*'
+			set_color green
+			echo "Using built-in $pip_version"
+			set cmd "sudo -H command pip $argv"
+		case '*'
+			set_color red
+			echo "Using another $pip_version"
+			set cmd "command pip $argv"
+	end
+
+	set_color normal
+	eval $cmd
+end
