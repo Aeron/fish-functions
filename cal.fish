52f3ab1db576b4bb4ce3049abbdb3eca4030ad3b
Eugene “Aeron” Glybin <aeron@aeron.cc>
2017-06-28 01:13:22 +0300

    Cal function added.

---
+++ cal.fish
@@ -0,0 +1,7 @@
+function cal -d 'Handy cal wrapper'
+	if [ -z "$argv" ]
+		command cal | grep --before-context 6 --after-context 6 --color -e '\ '(date +%e)'\ '
+	else
+		command cal $argv
+	end
+end
