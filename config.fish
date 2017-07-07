521614523f6c3f33c1f9a97d5eb99cd6ef1007ac
Eugene “Aeron” Glybin <aeron@aeron.cc>
2017-07-07 07:05:05 +0300

    Config functions updated.

--- config.fish
+++ config.fish
@@ -1,15 +1,37 @@
-function __config_edit
-	eval $EDITOR ~/.config/fish/config.fish
-end
+begin
+	set _home '~/.config/fish'
+	set _functions (string join '/' $_home 'functions')
 
-function __config_reload
-	source ~/.config/fish/config.fish
-end
+	set _config (string join '/' $_home 'config.fish')
+	set _prompt (string join '/' $_functions 'fish_prompt.fish')
 
-function __config_prompt
-	eval $EDITOR ~/.config/fish/functions/fish_prompt.fish
-end
+	set EDITOR_DIRS (string replace w '' $EDITOR)
 
-function config -a cmd -d 'Handly alias to manage fish config'
-	eval __config_$cmd
+	function __config_edit
+		eval $EDITOR $_config
+	end
+
+	function __config_reload
+		source $_config
+	end
+
+	function __config_prompt
+		eval $EDITOR $_prompt
+	end
+
+	function __config_home
+		eval $EDITOR_DIRS $_home
+	end
+
+	function __config_functions
+		eval $EDITOR_DIRS $_functions
+	end
+
+	function config -a cmd -d 'Handly alias to manage fish config'
+		if [ -z $cmd ]
+			__config_home
+		else
+			eval __config_$cmd
+		end
+	end
 end
