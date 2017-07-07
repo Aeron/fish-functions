7c2a53f4e728d468a7c2b532389f60f3ef3daf86
Eugene “Aeron” Glybin <aeron@aeron.cc>
2017-07-08 01:35:39 +0300

    Pip wrapper refactored/updated.

--- pip.fish
+++ pip.fish
@@ -1,15 +1,13 @@
-function pip -d 'Handy wrapper for macOS/homebrew pip'
-	set -l pip_version (command pip --version)
-	set -l cmd
+function pip -d 'Handy wrapper for macOS/Homebrew pip'
+	set pip_version (command pip --version)
+	set cmd
 
 	switch $pip_version 
 		case '* /Library*'
-			set_color green
-			echo "Using built-in $pip_version"
+			echo -s (set_color green) "Using built-in $pip_version"
 			set cmd "sudo -H command pip $argv"
 		case '*'
-			set_color yellow
-			echo "Using another $pip_version"
+			echo -s (set_color yellow) "Using another $pip_version"
 			set cmd "command pip $argv"
 	end
 
