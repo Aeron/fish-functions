afc176d8121d473e7b3e839040a8cf6a8e0e08e4
Eugene “Aeron” Glybin <aeron@aeron.cc>
2017-07-08 01:04:39 +0300

    Curltime function updated.

--- curltime.fish
+++ curltime.fish
@@ -1,3 +1,10 @@
-function curltime -a url -d "Measures request time"
-	set -l time (command curl -w '%{time_total}' -o /dev/null -s $url | string replace -a '0,' '')
+function curltime -a url -d "Measures request time via cURL request"
+	if test -z "$url"
+		echo -s (set_color $fish_color_error) 'URL is required' (set_color normal)
+		return 1
+	end
+
+	set time (command curl -w '%{time_total}' -o /dev/null -s "$url" | string replace ',' '.')
+
+	echo -s (math -s3 "$time * 1000" | string split '.')[1] 'ms'
 end
