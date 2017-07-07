fc4337f63f8b8ea3e3bffc33bb6cf93073dab160
Eugene “Aeron” Glybin <aeron@aeron.cc>
2017-07-07 23:53:45 +0300

    Newpy functions updated.

--- newpy.fish
+++ newpy.fish
@@ -1,42 +1,42 @@
 begin
-	set _default_python 'python3'
-	set _default_readme 'README.md'
-	set _default_requirements 'requirements.txt'
-	set _default_activate './bin/activate.fish'
+	set newpy_default_python 'python3'
+	set newpy_default_readme 'README.md'
+	set newpy_default_requirements 'requirements.txt'
+	set newpy_default_activate './bin/activate.fish'
 
-	set _package_regex '^[a-z]+$'
-	set _gitignore '.gitignore'
-	set _python_gitignore 'https://github.com/github/gitignore/raw/master/Python.gitignore'
-	set _venv_gitignore 'bin/' 'include/' 'man/' 'pyvenv.cfg'
-	set _git_initial 'Initial commit.'
+	set newpy_package_regex '^[a-z]+$'
+	set newpy_gitignore '.gitignore'
+	set newpy_python_gitignore 'https://github.com/github/gitignore/raw/master/Python.gitignore'
+	set newpy_venv_gitignore 'bin/' 'include/' 'man/' 'pyvenv.cfg'
+	set newpy_git_initial 'Initial commit.'
 
-	function __create_venv -a python
+	function _newpy_create_venv -a python
 		if test -z "$python"
-			set python $_default_python
+			set python $newpy_default_python
 		end
 
 		set python (which $python)
 
 		eval $python -m venv .
-		source $_default_activate
+		source $newpy_default_activate
 
-		echo -e '\n# venv' >> $_gitignore
-		echo -e (string join '\n' $_venv_gitignore) >> $_gitignore
+		echo -e '\n# venv' >> $newpy_gitignore
+		echo -e (string join '\n' $newpy_venv_gitignore) >> $newpy_gitignore
 	end
 
-	function __create_git
+	function _newpy_create_git
 		git init -q .
 		git add --all
-		git commit -qam $_git_initial
+		git commit -qam $newpy_git_initial
 	end
 
-	function newpy -a project package python no_venv no_git -d "Helps to setup a new Python project and its venv"
+	function newpy -a project package python -d "Helps to setup a new Python project, its venv and Git repo"
 		if test -z "$project" -o "$project" = '.'
 			set project (basename (pwd))
 		else
 			if test -d $project
-				echo -n (set_color $fish_color_error) "Directory '$project' already exists" (set_color normal)
-				exit 1
+				echo -s (set_color $fish_color_error) "Directory '$project' already exists" (set_color normal)
+				return 1
 			end
 		end
 
@@ -44,9 +44,9 @@ begin
 			set package (string trim (string split '-' "$project")[1])
 		end
 
-		if not string match -aq -r $_package_regex "$package"
-			echo -n (set_color $fish_color_error) "Package name '$package' doesn't comply with PEP8" (set_color normal)
-			exit 2
+		if not string match -aq -r $newpy_package_regex "$package"
+			echo -s (set_color $fish_color_error) "Package name '$package' doesn't comply with PEP8" (set_color normal)
+			return 1
 		end
 
 		if test "$project" != (pwd)
@@ -54,20 +54,20 @@ begin
 			cd $project
 		end
 
-		touch $_default_readme $_default_requirements
-		echo "# $package" >> $_default_readme
+		touch $newpy_default_readme $newpy_default_requirements
+		echo "# $package" >> $newpy_default_readme
 
-		curl -Lso $_gitignore $_python_gitignore
+		curl -Lso $newpy_gitignore $newpy_python_gitignore
 
 		mkdir $package
 		touch $package/__init__.py
 
-		if test -z "$no_venv"
-			__create_venv
+		if not contains -- --no-venv $argv
+			_newpy_create_venv $python
 		end
 
-		if test -z "$no_git"
-			__create_git
+		if not contains -- --no-git $argv
+			_newpy_create_git
 		end
 	end
 end
