68827f5a9faebff91d9d1a361abb73d7f7debe65
Eugene “Aeron” Glybin <aeron@aeron.cc>
2019-10-04 03:45:48 +0300

    Update source-posix.fish

--- source-posix.fish
+++ source-posix.fish
@@ -1,5 +1,13 @@
 function source-posix -a file -d "Exports variables from POSIX-compatible environment file"
+	set sep_comment '#'
+	set sep_value '='
+
 	for line in (cat $file)
-		set -gx (string split -m1 '=' $line)
+		set line (string split -m1 $sep_value (string split -r -m1 $sep_comment $line)[1])
+
+		if test -n "$line[1]" -a -n "$line[2]"
+			# echo $line
+			set -gx $line[1] $line[2]
+		end
 	end
 end
