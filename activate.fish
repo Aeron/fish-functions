4b571938b08abfc046bfcf3b92fde602fc28330c
Eugene “Aeron” Glybin <aeron@aeron.cc>
2019-10-04 04:25:59 +0300

    Update activate.fish

--- activate.fish
+++ activate.fish
@@ -1,12 +1,12 @@
-function activate -a quiet -d "Activates Python's venv for current directory"
+function activate -d "Activates Python's venv for current directory"
 	if test -e $VIRTUAL_ENV_DIR
 		source (string join / $PWD $VIRTUAL_ENV_DIR 'bin/activate.fish')
 
-		if test -z "$quiet"
+		if contains -- -v $argv
 			echo -s (set_color green) "Python's venv for $PWD activated" (set_color normal)
 		end
 	else
-		if test -z "$quiet"
+		if contains -- -v $argv
 			echo (set_color red) "There's no venv found in current directory" (set_color normal)
 		end
 	end
--- venv.fish
+++ venv.fish
@@ -3,7 +3,7 @@ function venv -d 'Handy wrapper for `python3 -m venv`'
 		set argv ".venv"
 	end
 
-	command python3 -m venv $argv;
-		and activate;
-		and pip install -U pip setuptools wheel
+	command python3 -m venv $argv
+	and activate -v
+	and pip install -U pip setuptools wheel
 end
