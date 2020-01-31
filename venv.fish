c9cde4e0355cf79ff4fc5ddd3cc3a0797d96cb2c
Eugene “Aeron” Glybin <aeron@aeron.cc>
2020-01-31 16:56:14 +0300

    Update venv.fish

--- venv.fish
+++ venv.fish
@@ -1,9 +1,38 @@
-function venv -d 'Handy wrapper for `python3 -m venv`'
-	if test -z "$argv"
-		set argv ".venv"
+begin
+	set default_dir ".venv"
+
+	function _remove_venv
+		if test -d $default_dir
+			rm -rf $default_dir
+		else
+			echo -s (set_color yellow) "No venv ($default_dir directory) found" (set_color normal)
+		end
 	end
 
-	command python3 -m venv $argv
-	and activate -v
-	and pip install -U pip setuptools wheel
+	function _create_venv
+		command python3 -m venv $default_dir
+		and activate -v
+		and pip install -U pip setuptools wheel
+	end
+
+	function _reset_venv
+		_remove_venv
+		_create_venv
+	end
+
+	function venv -d 'Handy wrapper for `python3 -m venv`'
+		if contains -- --rm $argv
+			_remove_venv
+		else
+			if contains -- --reset $argv
+				_reset_venv
+			else
+				_create_venv
+			end
+
+			# if contains -- --install-requirements $argv
+			# 	pip install -r ./requirements.txt
+			# end
+		end
+	end
 end
