daf9a293e31f1fb52ee2d37f1d323ca3128f8e99
Eugene “Aeron” Glybin <aeron@aeron.cc>
2020-04-01 15:31:57 +0300

    Pbcopy-GPG function create

---
+++ pbcopy-gpg.fish
@@ -0,0 +1,9 @@
+function pbcopy-gpg -a fingerprint -d "Copies a GPG public key"
+	if test -z "$fingerprint"
+		set fingerprint $GPG_KEY_FINGERPRINT
+	end
+
+    gpg2 --armor --export $fingerprint | pbcopy
+
+	echo "GPG public key copied"
+end
