a2cbeabaf49261292a77d8784241c5dede5b6053
Eugene “Aeron” Glybin <aeron@aeron.cc>
2020-04-01 15:28:35 +0300

    Pbcopy-SSH function update

--- pbcopy-key.fish
+++
@@ -1,7 +0,0 @@
-function pbcopy-key -a type -d "Copies an SSH public key"
-	if test -z "$type"
-		set type "ed25519"
-	end
-
-    cat ~/.ssh/id_$type.pub | pbcopy
-end
---
+++ pbcopy-ssh.fish
@@ -0,0 +1,9 @@
+function pbcopy-ssh -a type -d "Copies an SSH public key"
+	if test -z "$type"
+		set type "ed25519"
+	end
+
+    cat ~/.ssh/id_$type.pub | pbcopy
+
+	echo "SSH public key copied"
+end
