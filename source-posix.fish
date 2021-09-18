8caaa4beeb1e8a7ac41728344ba3b6d024d636a9
Eugene “Aeron” Glybin <aeron@aeron.cc>
2021-09-18 15:42:17 +0300

    Update source-posix.fish

--- source-posix.fish
+++ source-posix.fish
@@ -1,8 +1,13 @@
 function source-posix -a file -d "Exports variables from a POSIX-compatible environment file"
 	set sep_comment '#'
 	set sep_value '='
+	set regex_comment '^\#.*$'
 
 	for line in (cat $file)
+		if string match -q -re $regex_comment $line
+			continue
+		end
+
 		set line (string split -m1 $sep_value (string split -r -m1 $sep_comment $line)[1])
 
 		if test -n "$line[1]" -a -n "$line[2]"
