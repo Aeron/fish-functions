ea6d8e32635a5c453ca23d8a91f6577577a688eb
Eugene “Aeron” Glybin <aeron@aeron.cc>
2022-03-23 23:24:28 +0700

    Update kube-ctx.fish

--- kube-ctx.fish
+++ kube-ctx.fish
@@ -1,11 +1,7 @@
-begin
-    function kube-context -a context -d "Displays or sets current Kube context"
-        if test -z $context
-            command kubectl config current-context
-        else
-            command kubectl config use-context $context
-        end
+function kube-ctx -a context -d "Displays or sets current Kube context"
+    if test -z $context
+        command kubectl config current-context
+    else
+        command kubectl config use-context $context
     end
-
-    alias kube-ctx kube-context
 end
