591694b6483b3f6c8896abd7b2ef7126896f49a9
Eugene “Aeron” Glybin <aeron@aeron.cc>
2022-03-23 23:31:18 +0700

    Update pip-reset.fish

--- pip-reset.fish
+++ pip-reset.fish
@@ -1,3 +1,3 @@
 function pip-reset -d 'Removes local Python packages, except for a few vital ones'
-    pip uninstall -y (pip freeze | rg --invert-match "pip|setuptools|wheel")
+    pip uninstall -y (pip freeze | grep --invert-match "pip|setuptools|wheel")
 end
