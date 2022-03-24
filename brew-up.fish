02b00e5189e70cd69e471ad6fe229c81706e4175
Eugene “Aeron” Glybin <aeron@aeron.cc>
2022-03-24 23:19:07 +0700

    Update brew-up.fish

--- brew-up.fish
+++ brew-up.fish
@@ -1,8 +1,13 @@
 function brew-up -d 'Updates Homebrew, upgrades and cleans up packages'
 	brew update &> /dev/null
-	and brew outdated
 	and brew upgrade
 	and brew upgrade --cask
-	and brew rm (brew leaves --installed-as-dependency --quiet)
-	and brew cleanup --prune=0
+
+	set leftovers (brew leaves --installed-as-dependency --quiet)
+
+	if count $leftovers
+		brew rm $leftovers
+	end
+
+	brew cleanup --prune=0
 end
