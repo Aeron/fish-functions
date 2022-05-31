090ba4a7b91fc4092a7cc4873892b599606e8955
Eugene “Aeron” Glybin <aeron@aeron.cc>
2022-06-01 00:12:49 +0700

    Update up-up.fish

--- up-up.fish
+++ up-up.fish
@@ -1,6 +1,10 @@
 function up-up -d 'Updates macOS software, Homebrew, asdf, and local Python packages'
     soft-up
-    and brew-up
-    and asdf-up
-    and pip-up
+    brew-up
+
+    if contains -- --all $argv
+        asdf-up
+    end
+
+    pip-up
 end
