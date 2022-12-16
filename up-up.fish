40870b47fb80c802979cf7db56404981a31183a0
Eugene “Aeron” Glybin <aeron@aeron.cc>
2022-12-16 21:21:48 +0500

    Update up-up.fish
    Adds more verbose output.

--- up-up.fish
+++ up-up.fish
@@ -1,10 +1,15 @@
 function up-up -d 'Updates macOS software, Homebrew, asdf, and local Python packages'
+    echo -s (set_color green) "Updating macOS software" (set_color normal)
     soft-up
+
+    echo -se (set_color green) "\nUpdating Homebrew packages" (set_color normal)
     brew-up
 
     if contains -- --all $argv
+        echo -se (set_color green) "\nUpdating ASDF packages" (set_color normal)
         asdf-up
     end
 
+    echo -se (set_color green) "\nUpdating Python packages" (set_color normal)
     pip-up
 end
