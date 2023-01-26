fee0bbe298dc7e12c3ab04158afd678d3024e0d7
Eugene “Aeron” Glybin <aeron@aeron.cc>
2023-01-26 16:06:00 +0500

    Update up-up.fish

--- up-up.fish
+++ up-up.fish
@@ -1,15 +1,21 @@
 function up-up -d 'Updates macOS software, Homebrew, asdf, and local Python packages'
-    echo -s (set_color green) "Updating macOS software" (set_color normal)
-    soft-up
-
-    echo -se (set_color green) "\nUpdating Homebrew packages" (set_color normal)
-    brew-up
-
-    if contains -- --all $argv
-        echo -se (set_color green) "\nUpdating ASDF packages" (set_color normal)
-        asdf-up
+    if contains -- --all $argv  # NOTE: it is hard to cancel
+        echo -s (set_color green) "Updating macOS software" (set_color normal)
+        soft-up
+    else
+        echo -s (set_color cyan) "Skipping macOS software" (set_color normal)
     end
 
-    echo -se (set_color green) "\nUpdating Python packages" (set_color normal)
+    echo -se (set_color green) "Updating Homebrew packages" (set_color normal)
+    brew-up
+
+    if contains -- --all $argv  # NOTE: it is not always needed
+        echo -se (set_color green) "Updating ASDF packages" (set_color normal)
+        asdf-up
+    else
+        echo -se (set_color cyan) "Skipping ASDF packages" (set_color normal)
+    end
+
+    echo -se (set_color green) "Updating Python packages" (set_color normal)
     pip-up
 end
