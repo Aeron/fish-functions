ea55ccc5a2ebd51d338e7a8875f876af8e25220b
Eugene “Aeron” Glybin <aeron@aeron.cc>
2023-06-08 17:01:19 +0500

    Update pip-up.fish

--- pip-up.fish
+++ pip-up.fish
@@ -9,14 +9,14 @@ function pip-up -d "Updates local Python packages"
         set default_requirements_path ~/.requirements.txt
     end
 
-    if test -n $VIRTUAL_ENV
+    if test -n "$VIRTUAL_ENV"
         echo -s (set_color $fish_color_error) "error: venv is active" (set_color normal)
         return 1
     end
 
     command pip install -U pip wheel setuptools
 
-    if test -n $default_requirements_path
+    if test -n "$default_requirements_path"
         command pip install -Ur $default_requirements_path
     else
         echo -s (set_color yellow) "$default_requirements_path not found" (set_color normal)
