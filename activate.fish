714d6f425f226d5af3ec5a504e8ea83a53f184cd
Eugene “Aeron” Glybin <aeron@aeron.cc>
2023-08-08 17:39:09 +0500

    Update activate.fish

--- activate.fish
+++ activate.fish
@@ -1,13 +1,31 @@
-function activate -d "Activates Python's venv for current directory"
-	if test -e $VIRTUAL_ENV_DIR
-		source (string join / $PWD $VIRTUAL_ENV_DIR 'bin/activate.fish')
+function activate -d 'Activates Python\'s venv in the current $PWD'
+	set default_dir $VIRTUAL_ENV_DIR
 
-		if contains -- -v $argv
-			echo -s (set_color green) "Python's venv for $PWD activated" (set_color normal)
+	if test -z "$default_dir"
+		set default_dir ".venv"
+	end
+
+	if test -d $default_dir
+		set script_path "$PWD/$default_dir/bin/activate.fish"
+
+		if test -e "$script_path"
+			source "$script_path"
+
+			if contains -- -v $argv
+				echo -s (set_color green) \
+					"Python's venv for $PWD activated" \
+					(set_color normal)
+			end
+		else
+			echo (set_color red) \
+				"Activation script not found: $script_path" \
+				(set_color normal)
 		end
 	else
 		if contains -- -v $argv
-			echo (set_color red) "There's no venv found in current directory" (set_color normal)
+			echo (set_color red) \
+				"There's no venv found in current directory" \
+				(set_color normal)
 		end
 	end
 end
