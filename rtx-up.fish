6e55a9093ab045365d94cec0c20df49322c964af
Eugene “Aeron” Glybin <aeron@aeron.cc>
2023-09-13 18:26:35 +0500

    Update rtx-up.fish

--- rtx-up.fish
+++ rtx-up.fish
@@ -4,8 +4,16 @@ function rtx-up -d 'Updates rtx packages'
         return
     end
 
-    for plugin in $RTX_GLOBAL_LATEST
-        rtx global $plugin@latest
+    if set -q $RTX_GLOBAL_LATEST
+        if test -f ~/.tool-versions
+            for plugin in $RTX_GLOBAL_LATEST
+                rtx global $plugin@latest
+            end
+        else
+            for plugin in $RTX_GLOBAL_LATEST
+                rtx use --global $plugin@latest
+            end
+        end
     end
 
     rtx install
