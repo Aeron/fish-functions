552b763055917dbbdf1b94f3c3cbdd4a9a35e841
Eugene “Aeron” Glybin <aeron@aeron.cc>
2023-12-10 21:57:54 +0100

    Update kick.fish
    Adds support for the rtx-venv command. Alters behavior of the lang option. Alters handling for a non-empty target directory. Adds path resolving for the target directory parameter.

--- kick.fish
+++ kick.fish
@@ -1,4 +1,5 @@
 begin
+	set languages python go rust
 	set python_gitignore 'https://github.com/github/gitignore/raw/main/Python.gitignore'
 	set go_gitignore 'https://github.com/github/gitignore/raw/main/Go.gitignore'
 
@@ -28,7 +29,9 @@ begin
 		touch tests/__init__.py
 
 		if not contains -- --no-venv $argv
-			if functions -q venv
+			if functions -q rtx-venv
+				rtx-venv
+			else if functions -q venv
 				venv
 			else
 				command python3 -m venv .venv
@@ -82,40 +85,59 @@ begin
 	end
 
     function _get_help
-        echo "Usage:"
-        echo "    kick [OPTS...] NAME [TARGET]"
-        echo ""
-        echo "Options:"
-        echo "    --lang=python    Creates a Python project"
-        echo "    --lang=go        Creates a Go project"
-        echo "    --lang=rust      Creates a Rust project"
-        echo ""
-        echo "    --lib            Specifies the project is a library"
-        echo ""
-        echo "    --no-git         Ignores Git VCS initialization"
-        echo "    --no-venv        Ignores Python virtual environment creation"
-        echo ""
-        echo "Parameters:"
-        echo "    NAME             A project name [required]"
-        echo "    TARGET           A target directory [default: '.']"
+        echo 'Usage:'
+        echo '    kick [OPTS...] NAME [TARGET]'
+        echo ''
+        echo 'Options:'
+        echo '    --lang=python    Creates a Python project [default]'
+        echo '    --lang=go        Creates a Go project'
+        echo '    --lang=rust      Creates a Rust project'
+        echo ''
+        echo '    --lib            Specifies the project is a library'
+        echo ''
+        echo '    --no-git         Ignores Git VCS initialization'
+        echo '    --no-venv        Ignores Python virtual environment creation'
+        echo ''
+        echo 'Parameters:'
+        echo '    NAME             A project name [required]'
+        echo '    TARGET           A target directory [default: "."]'
     end
 
 	function kick -d "Kickstarts a new software development project"
-		argparse --ignore-unknown 'h/help' 'l/lang=?' -- $argv
+		set -l options
+		set options $options (fish_opt -s h -l help)
+		set options $options (fish_opt -s l -l lang --optional-val)
+		set options $options (fish_opt -s L -l lib --long-only)
+		set options $options (fish_opt -s G -l no-git --long-only)
+		set options $options (fish_opt -s V -l no-venv --long-only)
+
+		argparse --ignore-unknown $options -- $argv
 
 		if test $_flag_help; or test -z "$argv"
 			echo -e \
-				"kick (short for kickstart) help you start a new" \
+				"$_ (short for kickstart) help you start a new" \
 				"software development project.\n"
 			_get_help
 			return 0
 		end
 
+		if test -z "$_flag_lang"
+			set _flag_lang $languages[1]
+			# read -P 'Choose a language [python|go|rust]: ' _flag_lang
+		end
+
+		if not contains "$_flag_lang" $languages
+			echo -s \
+				(set_color $fish_color_error) \
+				'error: no suitable language specified; must be one of these: ' \
+				(string join ', ' $languages) \
+				(set_color normal)
+			return 1
+		end
+
 		# HACK: `function -a <args>` does not quite work here
 		# since all options goes to named params first
 		set params (string match --invert -- '-*' $argv)
-		set flags (string match -- '-*' $argv)
-
 		set name $params[1]
 		set target $params[2]
 
@@ -128,41 +150,27 @@ begin
 			return 1
 		end
 
-		set current_dir (basename (pwd))
+		set target (path resolve "$target")
 
-		if test -z "$target" -o "$target" = '.'
-			set target $current_dir
-		end
-
-		if test -z "$_flag_lang"
-			read -P 'Choose a language [python|go|rust]: ' _flag_lang
-		end
-
-		if not contains $_flag_lang python go rust
-			echo -s \
-				(set_color $fish_color_error) \
-				"error: no suitable language specified; must be python, go, or rust" \
-				(set_color normal)
-			return 1
-		end
-
-		if test "$target" != "$current_dir"
-			if test -d $target -a -n "$(command ls -1qA)"
-				echo -s \
-					(set_color $fish_color_error) \
-					"error: directory '$target' exists, and it is not empty" \
-					(set_color normal)
-				return 1
+		if test (ls -1A "$target" 2> /dev/null | wc -l | string trim) -gt 0
+			read -lun1 -P 'directory is not empty; continue anyway? [y|N] ' answer
+			if test "$answer" != 'y'
+				return 0
 			end
-
-			mkdir $target
+		else if test "$target" != "$PWD"
+			mkdir -p $target
 			and cd $target
 		end
 
-		switch $_flag_lang
-			case python; _create_python $name $flags
-			case go; _create_go $name $flags
-			case rust; _create_rust $name $flags
+		set flags $_flag_lib $_flag_no_git $_flag_no_venv
+
+		switch "$_flag_lang"
+			case python
+				_create_python $name $flags
+			case go
+				_create_go $name $flags
+			case rust
+				_create_rust $name $flags
 		end
 
 		if test $status -ne 0
@@ -173,7 +181,7 @@ begin
 
 		echo "# $name" > README.md
 
-		if not contains -- --no-git $argv; and test -d .git
+		if test -z "$_flag_no_git"; and test -d .git
 			git add --all
 		end
 	end
