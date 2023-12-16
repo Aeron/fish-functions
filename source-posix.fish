b1fcd222991675fe342215cd8c88565591d6c773
Eugene “Aeron” Glybin <aeron@aeron.cc>
2023-12-16 18:52:58 +0100

    Update source-posix.fish

--- source-posix.fish
+++ source-posix.fish
@@ -1,21 +1,20 @@
-function source-posix -a file -d "Exports variables from a POSIX-compatible environment file"
-	set sep_comment '#'
-	set sep_value '='
-	set regex_comment '^\#.*$'
+function source-posix -a file \
+    -d 'Exports variables from a POSIX-compatible environment file'
 
-	for line in (cat $file)
-		if string match -q -re $regex_comment $line
-			continue
-		end
+    for line in (cat $file)
+        if string match -q -re '^\#.*$' $line
+            continue
+        end
 
-		set line (string split -m1 $sep_value (string split -r -m1 $sep_comment $line)[1])
+        set line (string split -m1 '=' (string split -r -m1 '#' $line)[1])
 
-		if test -n "$line[1]" -a -n "$line[2]"
-			if contains -- -v $argv
-				echo $line
-			end
+        # TODO: do we always need an explicit value?
+        if test -n "$line[1]" -a -n "$line[2]"
+            if contains -- -v $argv
+                echo $line
+            end
 
-			set -gx $line[1] $line[2]
-		end
-	end
+            set -gx $line[1] $line[2]
+        end
+    end
 end
