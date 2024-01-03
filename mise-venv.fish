5c9f652ee68f4040501c2130513618c6fc2fc720
Eugene “Aeron” Glybin <aeron@aeron.cc>
2024-01-03 20:09:00 +0300

    rtx renaming update
    Updates the configuration file name.

--- mise-venv.fish
+++ mise-venv.fish
@@ -1,13 +1,13 @@
 function mise-venv \
     -d 'Configures a local mise-en-place (former rtx) Python with a virtual environment'
 
-    if test -e .rtx.toml
+    if test -e .mise.toml
         echo -s \
             (set_color $fish_color_error) \
-            'File .rtx.toml already exists; Better to edit it manually' \
+            'File .mise.toml already exists; Better to edit it manually' \
             (set_color normal)
         return
     end
 
-    echo -ne '[tools]\npython = {version="latest", virtualenv=".venv"}\n' > .rtx.toml
+    echo -ne '[tools]\npython = {version="latest", virtualenv=".venv"}\n' > .mise.toml
 end
