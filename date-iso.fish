9092157f06e0c9566708ddf9cd006a48488bbbc4
Eugene “Aeron” Glybin <aeron@aeron.cc>
2024-01-22 01:47:16 +0100

    Date ISO function update
    Adds the --short option that displays only ISO date without time.

--- date-iso.fish
+++ date-iso.fish
@@ -1,4 +1,8 @@
 function date-iso -d "Displays the current date in ISO8601"
+    if contains -- --short $argv
+        command date +%Y-%m-%d
+        return
+    end
     # For +%Y-%m-%dT%T.%N%z format, BSD systems do not support %N yet
     command date +%Y-%m-%dT%T%z
 end
