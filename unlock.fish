0827994e07d9cc05fb651d8ff8f3701b7cd522dc
Eugene “Aeron” Glybin <aeron@aeron.cc>
2024-01-23 01:15:59 +0100

    Unlock function update
    Removes unnecessary sudo. Adds option to sign.

--- unlock.fish
+++ unlock.fish
@@ -1,6 +1,10 @@
 function unlock -a path -d 'Disables macOS Gatekeeper for a specified application'
     if test -d $path
-        sudo xattr -rd com.apple.quarantine $path
+        xattr -rd com.apple.quarantine $path
+
+        if contains -- --sign $argv
+            codesign --force --deep --sign - $path
+        end
     else
         echo -s \
             (set_color $fish_color_error) \
