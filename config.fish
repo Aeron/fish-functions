1589da13b13eb1476770d53da6297fb3c254253a
Eugene “Aeron” Glybin <aeron@aeron.cc>
2024-05-07 03:16:41 +0200

    Config function update
    Adds kubecli and helix support. Improves the help description.

--- config.fish
+++ config.fish
@@ -10,6 +10,8 @@ function config -a utility -d 'Manages various utility configurations'
             eval $EDITOR_DIR ~/.config/fish
         case 'jj'
             jj config edit --user
+        case 'hx' 'helix'
+            eval $EDITOR_DIR ~/.config/helix
         case 'lsd'
             eval $EDITOR_DIR ~/.config/lsd
         case 'neovim' 'nvim'
@@ -18,6 +20,8 @@ function config -a utility -d 'Manages various utility configurations'
             eval $EDITOR_DIR ~/.config/mise
         case 'starship'
             eval $EDITOR ~/.config/starship.toml
+        case 'kube' 'kubecli'
+            eval $EDITOR ~/.kube/config
         case '*'
             echo 'Manage various utility configurations.'
             echo ''
@@ -25,12 +29,14 @@ function config -a utility -d 'Manages various utility configurations'
             echo ''
             echo 'Utilities:'
             echo '  alacritty    Opens the Alacritty config directory'
-            echo '  bottom/btm   Opens the bottom config file'
+            echo '  bottom, btm  Opens the bottom config file'
             echo '  fish         Opens the Fish config directory'
             echo '  jj           Opens the jj config file'
+            echo '  hx, helix    Opens the Helix config directory'
+            echo '  kube[cli]    Opens the kubecli config file'
             echo '  lsd          Opens the lsd config directory'
-            echo '  neovim/nvim  Opens the Neovim config directory'
-            echo '  rtx/mise     Opens the mise-en-place (former rtx) config directory'
+            echo '  n[eo]vim     Opens the Neovim config directory'
+            echo '  rtx, mise    Opens the mise-en-place (former rtx) config directory'
             echo '  starship     Opens the Starship config file'
             echo ''
             echo 'Parameters:'
