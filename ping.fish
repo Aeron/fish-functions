dc9fa3cac0b76cc5230b79f8398e5c5f0a33a26f
Eugene “Aeron” Glybin <aeron@aeron.cc>
2024-09-06 09:27:47 +0200

    Ping function update
    Changes the default host address. Introduces a relevant env variable support.

--- ping.fish
+++ ping.fish
@@ -1,6 +1,11 @@
-function ping -w ping -d 'A wrapper around the ping with a default address'
+function ping -w ping -d 'A wrapper around the ping with a default host address'
     if test -z $argv
-        command ping 1.1.1.1
+        if test -z $PING_DEFAULT_HOST
+            # set PING_DEFAULT_HOST 1.1.1.1
+            set PING_DEFAULT_HOST 9.9.9.9
+        end
+
+        command ping $PING_DEFAULT_HOST
     else
         command ping $argv
     end
