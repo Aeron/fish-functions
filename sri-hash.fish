7649306f2622a646d32e17c05762c78f77d340d0
Eugene “Aeron” Glybin <aeron@aeron.cc>
2025-12-14 11:11:53 +0100

    Minor update

--- sri-hash.fish
+++ sri-hash.fish
@@ -1,6 +1,9 @@
 function sri-hash -d 'Calculates subresource integrity hash'
     if test -z $argv[1]
-        echo -s (set_color $fish_color_error) 'error: valid file path is required' (set_color normal)
+        echo -s \
+            (set_color $fish_color_error) \
+            'error: valid file path is required' \
+            (set_color normal)
         return 1
     end
 
